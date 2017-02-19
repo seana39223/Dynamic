65dad9fa5b8ad11f2fa0f062cca14947771dd9ff
Sean Anderson <seana39223@gmail.com>
2017-02-19 21:04:53 +0000

    Spacing changed to improve human readability

--- API/login.php
+++ API/login.php
@@ -5,23 +5,26 @@ $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error) {
     die ("Connection to the database failed." . $connection->connect_error);
 }
+
 $email = mysqli_real_escape_string($connection, $_GET['email']);
 $pass = mysqli_real_escape_string($connection, $_GET['pass']);
 $sql = "SELECT * FROM users WHERE email = '$email' and password = '$pass'";
 $result = $connection->query($sql);
+
 if($result->num_rows==0) {
     die ("Password and username are not correct");
 }
 else {
     $encode = array();
-        while($row = mysqli_fetch_assoc($result)) {
+    while($row = mysqli_fetch_assoc($result)) {
         $encode[] = array (
             'firstname' => $row['firstname'],
             'lastname' => $row['lastname'],
             'email' => $row['email']
-            );
+        );
     }
 }
+
 echo json_encode($encode);
 $connection->close();
 ?>
