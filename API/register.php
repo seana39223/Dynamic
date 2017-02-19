6b7cadbd03ff970a0b13970459fa5b7730a968f0
Sean Anderson <seana39223@gmail.com>
2017-02-19 21:08:54 +0000

    More human readability changes

--- API/register.php
+++ API/register.php
@@ -1,19 +1,23 @@
 <?php
 //TODO: Update and add password security.
 //Below code is based of code from https://www.w3schools.com/php/php_mysql_insert.asp
-include "config.php"
+include "config.php";
 $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
+
 if ($connection->connect_error) {
 	die ("Connection to the database failed." . $connection->connect_error);
 }
+
 $firstName = mysqli_real_escape_string($connection, $_GET['firstName']);
 $lastName = mysqli_real_escape_string($connection, $_GET['lastName']);
 $email = mysqli_real_escape_String($connection, $_GET['email']);
 $type = mysqli_real_escape_String($connection, $_GET['type']);
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	$sql = "INSERT into users (firstname, lastname, email, type) VALUES ('$firstName', '$lastName', '$email', $type)";
-    	if ($connection->query($sql) === TRUE) {
+	$sql = "INSERT into users (firstname, lastname, email, type) 
+    VALUES ('$firstName', '$lastName', '$email', $type)";
+
+    if ($connection->query($sql) === TRUE) {
        	echo "New user added succesfully";
     } 
     else {
