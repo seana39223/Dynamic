5e4041b079a58bc4406d15d977ac7768e2d234ce
Sean Anderson <seana39223@gmail.com>
2017-02-19 20:51:26 +0000

    Code now tested and works, will have to update at a later data to allow for passwords to change.

--- API/login.php
+++ API/login.php
@@ -1,25 +1,24 @@
 <?php
 //Below code is based of code from http://www.kodingmadesimple.com/2015/01/convert-mysql-to-json-using-php.html
-include "config.php"
+include "config.php";
 $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error) {
-	die ("Connection to the database failed." . $connection->connect_error);
+    die ("Connection to the database failed." . $connection->connect_error);
 }
 $email = mysqli_real_escape_string($connection, $_GET['email']);
 $pass = mysqli_real_escape_string($connection, $_GET['pass']);
-$sql = "SELECT * FROM users WHERE email = $email and pass = $pass";
+$sql = "SELECT * FROM users WHERE email = '$email' and password = '$pass'";
 $result = $connection->query($sql);
-	
 if($result->num_rows==0) {
-	die "Password and username are not correct";
+    die ("Password and username are not correct");
 }
 else {
     $encode = array();
         while($row = mysqli_fetch_assoc($result)) {
         $encode[] = array (
-            'firstname' = $row['firstname'],
-            'lastname' = $row['lastname'],
-            'email' = $row['email']
+            'firstname' => $row['firstname'],
+            'lastname' => $row['lastname'],
+            'email' => $row['email']
             );
     }
 }
@@ -27,4 +26,4 @@ echo json_encode($encode);
 $connection->close();
 ?>
 
-			
+            
