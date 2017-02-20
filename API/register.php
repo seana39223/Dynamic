531bb6b087bf380d55ae5057bf689e353a5ade24
Sean Anderson <seana39223@gmail.com>
2017-02-20 13:58:26 +0000

    Added password hashing

--- API/register.php
+++ API/register.php
@@ -5,23 +5,25 @@ include "config.php";
 $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
 
 if ($connection->connect_error) {
-	die ("Connection to the database failed." . $connection->connect_error);
+    die ("Connection to the database failed." . $connection->connect_error);
 }
 
 $firstName = mysqli_real_escape_string($connection, $_POST['firstName']);
 $lastName = mysqli_real_escape_string($connection, $_POST['lastName']);
 $email = mysqli_real_escape_String($connection, $_POST['email']);
 $type = mysqli_real_escape_String($connection, $_POST['type']);
+$password = mysqli_real_escape_String($connection, $_POST['password']);
+$password = password_hash($password, PASSWORD_DEFAULT);
 
 if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-	$sql = "INSERT into users (firstname, lastname, email, type) 
-    VALUES ('$firstName', '$lastName', '$email', $type)";
+    $sql = "INSERT into users (firstname, lastname, email, type, password) 
+    VALUES ('$firstName', '$lastName', '$email', $type, '$password')";
 
     if ($connection->query($sql) === TRUE) {
-       	echo "New user added succesfully";
+        echo "New user added succesfully";
     } 
     else {
-    	echo "Error: " . $sql . "</br>" . $conn->error;
+        echo "Error: " . $sql . "</br>" . $conn->error;
     }
 }
 
@@ -32,4 +34,4 @@ else {
 $connection->close();
 ?>
 
-			
+            
