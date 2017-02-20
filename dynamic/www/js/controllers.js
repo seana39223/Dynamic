56a8e2ff6457d52ac734b9c636c1399d234ef824
Sean Anderson <seana39223@gmail.com>
2017-02-20 11:58:54 +0000

    AppCtrl renamed to LoginCtrl as more appropriate

--- dynamic/www/js/controllers.js
+++ dynamic/www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+.controller('LoginCtrl', function($scope, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -30,14 +30,11 @@ angular.module('starter.controllers', [])
   };
 
   // Perform the login action when the user submits the login form
+  //Used http://www.nikola-breznjak.com/blog/codeproject/posting-data-from-ionic-app-to-php-server/?ckattempt=1 as guide point for following code.
   $scope.doLogin = function() {
+    var api = "http://seananderson.co.uk/api/login.php";
     console.log('Doing login', $scope.loginData);
 
-    // Simulate a login delay. Remove this and replace with your login
-    // code if using a login system
-    $timeout(function() {
-      $scope.closeLogin();
-    }, 1000);
   };
 })
 
