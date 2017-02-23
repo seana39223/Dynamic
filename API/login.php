e549c1a416b27b589afa0b5e2e4ec86ff60ff51a
Sean Anderson <seana39223@gmail.com>
2017-02-23 13:29:14 +0000

    Updated so that the login file can now handle Angular POSTS

--- API/login.php
+++ API/login.php
@@ -1,13 +1,23 @@
 <?php
 //TODO: Update to be much more secure when necessary.
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
 include "config.php";
 $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error) {
     die ("Connection to the database failed." . $connection->connect_error);
 }
 
+//Following two lines convert Post Data into suitable format if Angular has ben used. Code taken from http://corpus.hubwiz.com/2/angularjs/15485354.html
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
 $email = mysqli_real_escape_string($connection, $_POST['email']);
 $pass = mysqli_real_escape_string($connection, $_POST['pass']);
+echo "Email is" . $email;
 $sql = "SELECT password FROM users WHERE email = '$email'";
 $user = $connection->query($sql);
 $array = $user->fetch_array(MYSQLI_ASSOC);
@@ -26,6 +36,7 @@ if (password_verify($pass, $correctPassword)) {
         );
     }
     echo json_encode($encode);
+    echo "YES YES YES IT HAS WORKED";
 }
 
 else {
@@ -37,4 +48,4 @@ $connection->close();
 ?>
 
             
-        
+            
