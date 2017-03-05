eda19f600a85d4c7591fef1175e3078dd601f085
Sean Anderson <seana39223@gmail.com>
2017-03-05 18:32:17 +0000

    Home controller changed so that it now gets JSON return from php with appropriate feeds

--- dynamic/www/js/controllers/home.js
+++ dynamic/www/js/controllers/home.js
@@ -1,3 +1,10 @@
 angular.module('home.controllers', [])
-.controller('HomeCtrl', function($scope) {
+.controller('HomeCtrl', function($scope, $http) {
+	var api = "http://seananderson.co.uk/api/feed.php";
+	var data = { 
+         email: localStorage.getItem('email')
+    }
+	$http.post(api, data).then(function(res){
+		console.log(res);
+    });
 });
