9c442295c353c6ffa1c057801d1a03afcd65ff14
Sean Anderson <seana39223@gmail.com>
2017-03-05 14:55:41 +0000

    Comments added to explain how file works

--- API/feed.php
+++ API/feed.php
@@ -13,10 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
     $_POST = json_decode(file_get_contents('php://input'), true);
 }
 
+//Below code gets the users id from the users email address.
 $email = mysqli_real_escape_string($connection, $_POST['email']);
 $sql = "SELECT id FROM users WHERE email = '$email'";
 $user = $connection->query($sql);
 $array = $user->fetch_array(MYSQLI_ASSOC);
 $id = $array['id'];
-var_dump($id);
-die();
