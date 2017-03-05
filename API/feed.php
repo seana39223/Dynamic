8854e340904d22b0d8f801f266c3b3193a83e251
Sean Anderson <seana39223@gmail.com>
2017-03-05 14:51:07 +0000

    Feed file updated so that it takes a users email and gets there id from db

---
+++ API/feed.php
@@ -0,0 +1,20 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+include "config.php";
+$connection = new mysqli($server, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error) {
+    die ("Connection to the database failed." . $connection->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
+$email = mysqli_real_escape_string($connection, $_POST['email']);
+$sql = SELECT "id from users WHERE email = '$email'";
+$user = $connection->query($sql);
+var_dump($user);
+die();
