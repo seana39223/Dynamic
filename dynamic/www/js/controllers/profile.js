9a646febba689cb1d50c52563986eb974a53aa39
Sean Anderson <seana39223@gmail.com>
2017-03-14 17:39:37 +0000

    Profile controller now allows for a photo to be taken or for one to be selected from file gallery

--- dynamic/www/js/controllers/profile.js
+++ dynamic/www/js/controllers/profile.js
@@ -1,3 +1,32 @@
-angular.module('profile.controllers', [])
-.controller('myProfileCtrl', function($scope) {
+angular.module('profile.controllers', ['ionic', 'ngCordova'])
+.controller('myProfileCtrl', function($scope, $cordovaCamera, $cordovaFile) {
+
+
+  //Below code for taking picture based of https://devdactic.com/how-to-capture-and-store-images-with-ionic/
+  $scope.images = [];
+ 
+  $scope.takePhoto = function(photo) {
+    if (photo=='new') {
+	  var options = {
+		quality:80,
+		sourceType : Camera.PictureSourceType.CAMERA, // Camera.PictureSourceType.PHOTOLIBRARY
+		encodingType: Camera.EncodingType.PNG,
+	  };
+	}
+
+	if (photo=='old') {
+      var options = {
+	    quality:80,
+		sourceType :  Camera.PictureSourceType.PHOTOLIBRARY, 
+		encodingType: Camera.EncodingType.PNG,
+	  }
+	}
+	
+	$cordovaCamera.getPicture(options).then(function(imageData) {
+	  alert(imageData);
+	}, 
+	function(err) {
+      console.log(err);
+	});
+  }
 });
