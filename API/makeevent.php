14158d38daa33fe30f4d9535bfd172932732bcac
Sean Anderson <seana39223@gmail.com>
2017-03-23 12:30:02 +0000

    Base code for make event created

---
+++ API/makeevent.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+include "config.php";
+$connection = new mysqli($server, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error) {
+    die ("Connection to the database failed." . $connection->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+}
+
+//Below code gets the users id from the users email address.
+$email = mysqli_real_escape_string($connection, $_POST['email']);
+$event_name = mysqli_real_escape_string($connection, $_POST['eventname']);
+$postcode = mysqli_real_escape_string($connection, $_POST['postcode']);
+$genre = mysqli_real_escape_string($connection, $_POST['genre']);
+
+$sql = "SELECT id FROM users WHERE email = '$email'";
+$user = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
+$array = $user->fetch_array(MYSQLI_ASSOC);
+$venue_owner_id = $array['id'];
