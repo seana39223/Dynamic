901161d89b831b042e174f4527cef192bd4d46da
Sean Anderson <seana39223@gmail.com>
2017-03-28 17:42:52 +0100

    List events php file created

---
+++ API/listevents.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+include "config.php";
+$connection = new mysqli($server, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error) {
+    die ("Connection to the database failed." . $connection->connect_error);
+}
+
+$sql = "SELECT * FROM users";
+$users = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
+$rows = array();
+while($user = mysqli_fetch_assoc($users)) {
+    $rows[] = $user;
+}
+print json_encode($rows);
