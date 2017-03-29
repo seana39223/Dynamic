d09b154998856c4e5f58c3a059393961270f04ae
Sean Anderson <seana39223@gmail.com>
2017-03-29 19:14:31 +0100

    Register genre now works properly

--- API/registergenre.php
+++ API/registergenre.php
@@ -12,30 +12,24 @@ if ($connection->connect_error) {
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
     $_POST = json_decode(file_get_contents('php://input'), true);
 }
-
-var_dump($_POST['genre']);
-die();
+$email = mysqli_real_escape_string($connection, $_POST['email']);
+$genre = mysqli_real_escape_string($connection, $_POST['genre']);
 
 //Below code gets the users id from the users email address.
-$email = mysqli_real_escape_string($connection, $_POST['email']);
 $sql = "SELECT id FROM users WHERE email = '$email'";
-$user = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
+$user = $connection->query($sql);
 $array = $user->fetch_array(MYSQLI_ASSOC);
 $id = $array['id'];
-$genreArray = (json_decode(json_encode($_POST['genre'])));
-var_dump($genreArray);
-die();
 
-foreach ($_genreArray as $genre) {
-    var_dump($genre);
-    die();
-    $sql = "GET id FROM genres WHERE name = $genre";
-    $genrequery = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
-    $array = $genrequery->getch_array(MSQLI_ASSOC);
-    $genreid = $array['id'];
-    $sql = "INSERT into user_genres (user_id, genre_id) VALUES ('$id', '$genreid')";
-    if ($connection->query($sql) === TRUE) {
-        echo "Genre added correctly";
-    } 
-}
-$connection->close();
+//Below code gets genre id from genre name
+$sql = "SELECT id FROM genres WHERE name = '$genre'";
+$genre = $connection->query($sql);
+$genreArray = $genre->fetch_array(MYSQLI_ASSOC);
+$genreId = $genreArray['id'];
+
+$sql = "INSERT into user_genres (user_id, genre_id) VALUES ('$id', '$genreId')";
+if ($connection->query($sql) === TRUE) {
+    echo "Genre added correctly";
+} 
+
+?>
