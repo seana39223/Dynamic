5f000de9d1255780cfbe3268df5e8bb5e6156670
Sean Anderson <seana39223@gmail.com>
2017-03-31 20:50:14 +0100

    Events can now be sorted, object isn't passed onto view as of yet

--- dynamic/www/js/controllers/events.js
+++ dynamic/www/js/controllers/events.js
@@ -5,10 +5,9 @@ angular.module('events.controllers', [])
     $scope.searchEvents = function() {
     	var api = "http://seananderson.co.uk/api/listevents.php";
     	$http.get(api).then(function(res){
-    		console.log(res['data']);
+    		//console.log(res['data']['postcode']);
     		$scope.listData=[];
             res['data'].forEach(function(event2) {
-            	console.log(event2);
           		$scope.eventsList.push(event2);
             })
     	});
@@ -18,30 +17,98 @@ angular.module('events.controllers', [])
     	}
 
     	else if ($scope.events.search == "Nearby Events") {
-    		//Below code uses plugin and is based  of http://ngcordova.com/docs/plugins/geolocation/
+            var eventsArray = new Array();
+            //Below code uses plugin and is based  of http://ngcordova.com/docs/plugins/geolocation/
             var posOptions = {timeout: 3000};
-    		$cordovaGeolocation .getCurrentPosition(posOptions) .then(function (position) {
+    		$cordovaGeolocation .getCurrentPosition(posOptions) .then(function(position) {
             var lat  = position.coords.latitude
             var long = position.coords.longitude
 
             $scope.eventsList.forEach(function(event) {
             	api = "http://api.postcodes.io/postcodes/" + event['postcode'];
+
+                var latEvent;
+                var longEvent;
             	$http.get(api).then(function(res) {
-            	    console.log(res['data']['result']['latitude']);
-            	    console.log(res['data']['result']['longitude'])
-                })
+            	  latEvent = (res['data']['result']['latitude']);
+            	  longEvent = (res['data']['result']['longitude']);
+                  api = "http://seananderson.co.uk/api/distance.php";
+                  data = {
+                    lat1: lat,
+                    lon1: long,
+                    lat2: latEvent,
+                    lon2: longEvent
+                  }
+                  $http.post(api, data).then(function(res) {
+                    miles = (res['data']);
+                    var eventInfo = [event, miles];
+                    eventsArray.push(eventInfo);
+                    //Below code is taken from http://stackoverflow.com/questions/3524827/sort-a-2d-array-by-the-second-value
+                    //Code works by comparing second element rather than first element in the array.
+                    eventsArray.sort(function(a,b){
+                      return a[1] - b[1];
+                    });
+                  })
+                });
             })
+            console.log(eventsArray);
+
         }, function(err) {
           alert("Error: Please make sure you have GPS enabled");
     });
     	}
 
     	else if ($scope.events.search == "Near Your Home") {
-    		console.log("Near your home selected");
+            var eventsArray = new Array();
+            var api = "http://seananderson.co.uk/api/getpostcode.php";
+            var data = {
+                email: localStorage.getItem('email')
+            }
+            $http.post(api, data).then(function(res) {
+                var homePostcode = res['data'];
+                api = "http://api.postcodes.io/postcodes/" + homePostcode ;
+                var latHome;
+                var longHome;
+                $http.get(api).then(function(res) {
+                  latHome = (res['data']['result']['latitude']);
+                  longHome = (res['data']['result']['longitude']);
+                  console.log(latHome);
+                  console.log(longHome); 
+                })
+                $scope.eventsList.forEach(function(event) {
+                api = "http://api.postcodes.io/postcodes/" + event['postcode'];
+
+                var latEvent;
+                var longEvent;
+                $http.get(api).then(function(res) {
+                  latEvent = (res['data']['result']['latitude']);
+                  longEvent = (res['data']['result']['longitude']);
+                  api = "http://seananderson.co.uk/api/distance.php";
+                  data = {
+                    lat1: latHome,
+                    lon1: longHome,
+                    lat2: latEvent,
+                    lon2: longEvent
+                  }
+                  $http.post(api, data).then(function(res) {
+                    miles = (res['data']);
+                    var eventInfo = [event, miles];
+                    eventsArray.push(eventInfo);
+                    //Below code is taken from http://stackoverflow.com/questions/3524827/sort-a-2d-array-by-the-second-value
+                    //Code works by comparing second element rather than first element in the array.
+                    eventsArray.sort(function(a,b){
+                      return a[1] - b[1];
+                    });
+                  })
+                });
+            })
+            console.log(eventsArray);
+
+            })
     	}
 
     	else if ($scope.events.search == "Date of Events") {
-    		console.log("Dates of event selected");
+    		console.log($scope.eventsList);
     	}
     }
 });
