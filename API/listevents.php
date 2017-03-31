3f4d53b8c2904960b44ed25f33a3babccdc7d0cb
Sean Anderson <seana39223@gmail.com>
2017-03-31 20:50:36 +0100

    List events is now done by date

--- API/listevents.php
+++ API/listevents.php
@@ -2,17 +2,17 @@
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
 header('Access-Control-Allow-Methods: GET, POST, PUT');
-
 include "config.php";
 $connection = new mysqli($server, $dbuser, $dbpass, $dbname);
 if ($connection->connect_error) {
     die ("Connection to the database failed." . $connection->connect_error);
 }
-
-$sql = "SELECT * FROM users";
-$users = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
+$lat = mysqli_escape_string($_POST['lat']);
+$long = mysqli_escape_string($_POST['long']);
+$sql = "SELECT * FROM events ORDER by date";
+$events = $connection->query($sql) or trigger_error($mysqli->error."[$sql]");
 $rows = array();
-while($user = mysqli_fetch_assoc($users)) {
-    $rows[] = $user;
+while($event = mysqli_fetch_assoc($events)) {
+    $rows[] = $event;
 }
-print json_encode($rows);
+print json_encode($rows);	
