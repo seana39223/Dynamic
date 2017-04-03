d796e6f25f9878a9b00b998deac27cab44470975
Sean Anderson <seana39223@gmail.com>
2017-04-03 20:35:28 +0100

    Lovers info now checks api to see if user follows user or not

--- dynamic/www/js/controllers/loversinfo.js
+++ dynamic/www/js/controllers/loversinfo.js
@@ -1,5 +1,6 @@
 angular.module('loversInfo.controllers', [])
 .controller('LoversInfoCtrl', function($scope, $http) {
+  $scope.$on("$ionicView.beforeEnter", function() {
   var url = window.location.href;
   var id = url.substring(url.indexOf("=") + 1);
   var api = "http://seananderson.co.uk/api/getloversinfo.php";
@@ -22,5 +23,26 @@ angular.module('loversInfo.controllers', [])
             postDiv.append('<p>'+ post['text'] + '</p>');
         })
 	})
+
+	var api = "http://seananderson.co.uk/api/checkfollowing.php";
+	var data = {
+		email: localStorage.getItem('email'),
+		id : id
+	}
+	$http.post(api, data).then(function(res) {
+		var apiResponse = (res['data']);
+		console.log(apiResponse);
+		if(apiResponse.indexOf("This user does not follow the current user")>=0){
+			postDiv.append('<button ng-click="test()">Follow</button>');
+		}
+		if(apiResponse.indexOf("The user does indeed follow already")>=0){
+			postDiv.append('<button ng-click="test()">Un Follow</button');
+		}
+	})
+  })
+
+  test = function(){
+  	console.log("test");
+  }
 
 });
