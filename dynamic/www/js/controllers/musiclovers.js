92e4e6dd294dabb1dda60aee7fad18461f0369ea
Sean Anderson <seana39223@gmail.com>
2017-04-03 16:15:18 +0100

    Music lovers controller updated

--- dynamic/www/js/controllers/musiclovers.js
+++ dynamic/www/js/controllers/musiclovers.js
@@ -2,19 +2,14 @@ angular.module('musiclovers.controllers', ['ionic'])
 .controller('musicLoversCtrl', function($scope, $http) {
   var test;
   var api = "http://seananderson.co.uk/api/listusers.php";
-  var feed = angular.element(document.querySelector('#users'));
+  var feed = angular.element(document.querySelector('#lovers'));
   var data = { 
     email: localStorage.getItem('email')
   }
   $http.post(api, data).then(function(res){
   	res['data'].forEach(function(user) {
         var userNumber = user['id'];
-  	    //feed.append('<tr ng-form="followUser'+userNumber+'">' + '<td>' + user['displayname'] + '</td><td><button class="button button-icon" id='+user['id'] +' ng-click="follow()" data='+ userNumber +'> Follow User</button></td></tr>');
-        feed.append('<table><tr ng-form="myForm"><td>' + user['displayname'] + '</td><td><button class="button button-icon" id='+user['id'] +' ng-click="follow()" data='+ userNumber +'> Follow User</button></td></tr></table>');
+        feed.append('<div id = " ' + userNumber + '" class="users">' + '<h2>' + user['displayname'] + '</h2> </br> <a href="#/app/loversinfo?lover=' + userNumber + '">More Info</a></div></br>');
   	});
   });
-  
-  $scope.follow = function(){
-    console.log(userNumber);
-  }
 });
