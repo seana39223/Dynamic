4d5279bc6fe95866381cc75483ffe36c0005bb54
Sean Anderson <seana39223@gmail.com>
2017-04-04 15:13:42 +0100

    Shared pop up controller added

--- dynamic/www/js/controllers/shared.js
+++ dynamic/www/js/controllers/shared.js
@@ -1,5 +1,5 @@
 angular.module('shared.controllers', [])
-.controller('AppCtrl', function($scope, $stateParams, $state) {
+.controller('AppCtrl', function($scope, $stateParams, $state, $ionicPopup) {
   //Logout function which can be called by any of the pages other than the register and login pages.
   $scope.logOut = function() {
   	localStorage.removeItem('email');
@@ -8,5 +8,15 @@ angular.module('shared.controllers', [])
     $ionicHistory.clearCache();
     $ionicHistory.nextViewOptions({ disableBack: true, historyRoot: true });
     $window.location.reload();
+  }
+
+    //Generic popup as there may need to be multiple popups for this page. Code is based of ionic documentation for popup.
+  popUp = function(title, message) {
+  	var alertPopup = $ionicPopup.alert({
+  		title: title,
+  		template: message
+  	});
+  	alertPopup.then(function(res) {
+  	});
   }
 });
