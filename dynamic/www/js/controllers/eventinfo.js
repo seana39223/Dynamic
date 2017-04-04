7a503b5683d9ae759a14151fc8b7d5f23ccd0f0d
Sean Anderson <seana39223@gmail.com>
2017-04-04 20:10:34 +0100

    Events info controlelr changed to have favoruite or un favoruite event

--- dynamic/www/js/controllers/eventinfo.js
+++ dynamic/www/js/controllers/eventinfo.js
@@ -26,6 +26,22 @@ angular.module('eventInfo.controllers', [])
 		}
 		eventDetails.append('<p>' + res['data']['city'] + '</p>');
 		eventDetails.append('<p>' + res['data']['postcode'] + '</p>');
+		var api = "http://seananderson.co.uk/api/checkfav.php";
+		var data = {
+			event_id: id,
+			email: localStorage.getItem('email')
+		}
+		$http.post(api,data).then(function(res){
+			console.log(res['data']);
+			var apiResult = JSON.stringify(res['data']);
+			if (apiResult.indexOf("This is not already a fav.")>=0) {
+			  eventDetails.append('<a href="#/app/eventfav?event=' + id + '">Add event to favoruites</a>');
+			}
+			else {
+				eventDetails.append('<a href="#/app/eventunfav?event=' + id + '">Remove event from favoruites</a>');
+			}
+		})
+
 	})
 
 });
