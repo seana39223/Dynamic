cf704d46d4e72e160ebe1b11e27f8c332f6a7e3d
Sean Anderson <seana39223@gmail.com>
2017-04-05 17:38:26 +0100

    Events can now be sorted by distance

--- dynamic/www/js/controllers/events.js
+++ dynamic/www/js/controllers/events.js
@@ -1,12 +1,28 @@
 angular.module('events.controllers', [])
-.controller('EventsCtrl', function($scope, $cordovaGeolocation, $http, $compile, $state) {
-	$scope.events = {};
-	$scope.eventsList = [];
+.controller('EventsCtrl', function($scope, $cordovaGeolocation, $http, $compile, $state, $ionicLoading) {
+
+    $scope.events = {};
+    $scope.eventsList = [];
+    var api = "http://seananderson.co.uk/api/listevents.php";
+    $http.get(api).then(function(res){
+      $scope.listData=[];
+      res['data'].forEach(function(event2) {
+        $scope.eventsList.push(event2);
+      })
+      $ionicLoading.show();
+      var eventDiv = angular.element(document.querySelector('#events'));
+      eventDiv.html('<div id="events"></div>');
+      $scope.eventsList.forEach(function(event) {
+        eventDiv.append('<div id= " ' + event['event_id'] + '" class="events"><h2>' + event['event_name'] + '</h2>' +'<p>' + event['event_name'] + '</p>' + '</br> <a href="#/app/eventinfo?event=' + event['event_id'] + '">More Info</a></div></br>');
+      })
+      $scope.eventsList.length = 0;
+      $ionicLoading.hide();
+      $scope.eventsList.length=0;
+    });
 
     $scope.searchEvents = function() {
     	var api = "http://seananderson.co.uk/api/listevents.php";
     	$http.get(api).then(function(res){
-    		//console.log(res['data']['postcode']);
     		$scope.listData=[];
             res['data'].forEach(function(event2) {
           		$scope.eventsList.push(event2);
@@ -18,13 +34,15 @@ angular.module('events.controllers', [])
     	}
 
     	else if ($scope.events.search == "Nearby Events") {
+            $ionicLoading.show();
+            var eventDiv = angular.element(document.querySelector('#events'));
+            eventDiv.html('<div id="events"></div>');
             var eventsArray = new Array();
             //Below code uses plugin and is based  of http://ngcordova.com/docs/plugins/geolocation/
             var posOptions = {timeout: 3000};
     		$cordovaGeolocation .getCurrentPosition(posOptions) .then(function(position) {
             var lat  = position.coords.latitude
             var long = position.coords.longitude
-
             $scope.eventsList.forEach(function(event) {
             	api = "http://api.postcodes.io/postcodes/" + event['postcode'];
 
@@ -44,14 +62,25 @@ angular.module('events.controllers', [])
                     miles = (res['data']);
                     var eventInfo = [event, miles];
                     eventsArray.push(eventInfo);
+                    var eventInfo =[];
                     //Below code is taken from http://stackoverflow.com/questions/3524827/sort-a-2d-array-by-the-second-value
                     //Code works by comparing second element rather than first element in the array.
                     eventsArray.sort(function(a,b){
                       return a[1] - b[1];
                     });
+                    var eventDiv = angular.element(document.querySelector('#events'));
+                    eventDiv.html('<div id="events"></div>');
+                    eventsArray.forEach(function(event) {
+                     eventDiv.append('<div id= " ' + event[0]['event_id'] + '" class="events"> <h2>' + event[0]['event_name'] + '</h2>' +'<p>' + event[0]['event_name'] + '</p>' + '</br> <a href="#/app/eventinfo?event=' + event[0]['event_id'] + '">More Info</a></div></br>');
+                    })
                   })
                 });
+                console.log(eventsArray);
             })
+            $scope.eventsList.length = 0;
+
+
+            $ionicLoading.hide();
 
         }, function(err) {
           alert("Error: Please make sure you have GPS enabled");
@@ -59,8 +88,6 @@ angular.module('events.controllers', [])
     	}
 
     	else if ($scope.events.search == "Near Your Home") {
-            var eventDiv = angular.element(document.querySelector('#events'));
-            eventDiv.html('<div id="events"></div>');
             var eventsArray = new Array();
             var api = "http://seananderson.co.uk/api/getpostcode.php";
             var data = {
@@ -74,8 +101,6 @@ angular.module('events.controllers', [])
                 $http.get(api).then(function(res) {
                   latHome = (res['data']['result']['latitude']);
                   longHome = (res['data']['result']['longitude']);
-                  console.log(latHome);
-                  console.log(longHome); 
                 })
                 $scope.eventsList.forEach(function(event) {
                 api = "http://api.postcodes.io/postcodes/" + event['postcode'];
@@ -101,20 +126,34 @@ angular.module('events.controllers', [])
                     eventsArray.sort(function(a,b){
                       return a[1] - b[1];
                     });
+                    var eventDiv = angular.element(document.querySelector('#events'));
+                    eventDiv.html('<div id="events"></div>');
+                    eventsArray.forEach(function(event) {
+                     eventDiv.append('<div id= " ' + event[0]['event_id'] + '" class="events"> <h2>' + event[0]['event_name'] + '</h2>' +'<p>' + event[0]['event_name'] + '</p>' + '</br> <a href="#/app/eventinfo?event=' + event[0]['event_id'] + '">More Info</a></div></br>');
+                    })
                   })
                 });
+                console.log(eventsArray);
             })
-            console.log(eventsArray);
+            $scope.eventsList.length = 0;
+
+
+            $ionicLoading.hide();
 
             })
     	}
 
     	else if ($scope.events.search == "Date of Events") {
+            $ionicLoading.show();
             var eventDiv = angular.element(document.querySelector('#events'));
+            eventDiv.html('<div id="events"></div>');
             $scope.eventsList.forEach(function(event) {
-                eventDiv.append('<div id= " ' + event['event_id'] + '" class="events"> <div class="list"><h2>' + event['event_name'] + '</h2>' +'<p>' + event['event_name'] + '</p>' + '</br> <a href="#/app/eventinfo?event=' + event['event_id'] + '">More Info</a></div></br>');
+                eventDiv.append('<div id= " ' + event['event_id'] + '" class="events"> <h2>' + event['event_name'] + '</h2>' +'<p>' + event['event_name'] + '</p>' + '</br> <a href="#/app/eventinfo?event=' + event['event_id'] + '">More Info</a></div></br>');
             })
+            $scope.eventsList.length = 0;
+            $ionicLoading.hide();
     	}
+        $scope.eventsList.length=0;
     }
 
 
