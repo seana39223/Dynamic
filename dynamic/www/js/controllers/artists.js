e98282e0deeca0f246cbea9fee7ec7dd55d35bcf
Sean Anderson <seana39223@gmail.com>
2017-04-07 14:33:14 +0100

    Now lists artists and can follow them

--- dynamic/www/js/controllers/artists.js
+++ dynamic/www/js/controllers/artists.js
@@ -1,3 +1,15 @@
 angular.module('artists.controllers', [])
-.controller('ArtistsCtrl', function($scope) {
+.controller('ArtistsCtrl', function($scope, $http) {
+  var api = "http://seananderson.co.uk/api/listartists.php";
+  var feed = angular.element(document.querySelector('#artists'));
+  var data = { 
+    email: localStorage.getItem('email')
+  }
+  $http.post(api, data).then(function(res){
+  	console.log(data);
+  	res['data'].forEach(function(user) {
+        var userNumber = user['id'];
+        feed.append('<div id = " ' + userNumber + '" class="users">' + '<h2>' + user['displayname'] + '</h2> </br> <a href="#/app/loversinfo?lover=' + userNumber + '">More Info</a></div></br>');
+  	});
+  });
 });
