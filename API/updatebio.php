bc30a8948a57775fc4c669a55a2b3dca024b310d
Sean Anderson <seana39223@gmail.com>
2017-04-13 22:17:37 +0100

    Updated bio API file added

---
+++ API/updatebio.php
@@ -0,0 +1,25 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT');
+
+include "config.php";
+$connection = new mysqli($server, $dbuser, $dbpass, $dbname);
+if ($connection->connect_error) {
+    die ("Connection to the database failed." . $connection->connect_error);
+}
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST' && empty($_POST)) {
+    $_POST = json_decode(file_get_contents('php://input'), true);
+}
+$dName = $_POST['dName'];
+$bio = mysqli_real_escape_string($connection, $_POST['bio']);
+$sql = "UPDATE users SET bio = '$bio' WHERE displayname = '$dName' ";
+
+if ($connection->query($sql) === TRUE) {
+    echo "Image location updated";
+} 
+else {
+    echo "Error: " . $sql . "</br>" . $conn->error;
+}
+?>
