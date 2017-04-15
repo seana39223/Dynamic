9d075b9022b7b39ec1f77915a8ba7b33cbd26ba4
Sean Anderson <seana39223@gmail.com>
2017-04-15 14:56:30 +0100

    Login unit tests started

--- dynamic/tests/unit-tests/login/logintest.js
+++ dynamic/tests/unit-tests/login/logintest.js
@@ -1,29 +1,26 @@
 describe('LoginController', function() {
 
     var controller, 
-        deferredLogin,
-        dinnerServiceMock,
+        loginServiceMock,
         stateMock,
         ionicPopupMock;
 
     beforeEach(module('app'));  
-
-    // TODO: Instantiate the Controller and Mocks
 
     describe('#doLogin', function() {
 
         // TODO: Call doLogin on the Controller
 
         it('should call login on dinnerService', function() {
-            expect(dinnerServiceMock.login).toHaveBeenCalledWith('test1', 'password1'); 
+            expect(loginServiceMock.login).toHaveBeenCalledWith('sea6@aber.ac.uk', 'b261la'); 
         });
 
-        describe('when the login is executed,', function() {
-            it('if successful, should change state to my-dinners', function() {
+        describe('when the login action is performed,', function() {
+            it('if successful, should change state to home', function() {
 
                 // TODO: Mock the login response from DinnerService
 
-                expect(stateMock.go).toHaveBeenCalledWith('my-dinners');
+                expect(stateMock.go).toHaveBeenCalledWith('app.home');
             });
 
             it('if unsuccessful, should show a popup', function() {
