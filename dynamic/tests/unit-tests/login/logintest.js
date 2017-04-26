f26ee15774b93e4806ea56afee94d1adf2220277
Sean Anderson <seana39223@gmail.com>
2017-04-26 15:43:54 +0100

    Login tests added

--- dynamic/tests/unit-tests/login/logintest.js
+++ dynamic/tests/unit-tests/login/logintest.js
@@ -1,34 +1,39 @@
-describe('LoginController', function() {
+describe('LoginCtrl', function() {
 
     var controller, 
-        loginServiceMock,
+        loginMock,
         stateMock,
         ionicPopupMock;
+    
+    beforeEach(module('app'));
 
-    beforeEach(module('app'));  
+    beforeEach(module('#'));  
 
     describe('#doLogin', function() {
 
-        // TODO: Call doLogin on the Controller
 
-        it('should call login on dinnerService', function() {
-            expect(loginServiceMock.login).toHaveBeenCalledWith('sea6@aber.ac.uk', 'b261la'); 
+        it('Should Login using correct details', function() {
+            expect(loginMock.doLogin).toHaveBeenCalledWith('sea6@aber.ac.uk', 'b261la'); 
         });
 
         describe('when the login action is performed,', function() {
             it('if successful, should change state to home', function() {
-
-                // TODO: Mock the login response from DinnerService
-
                 expect(stateMock.go).toHaveBeenCalledWith('app.home');
             });
 
             it('if unsuccessful, should show a popup', function() {
 
-                // TODO: Mock the login response from DinnerService
-
                 expect(ionicPopupMock.alert).toHaveBeenCalled();
             });
         });
     })
+
+    describe('#register', function(){
+
+        it('Should go to register screen', function(){
+             expect(stateMock.go).toHaveBeenCalledWith('register');
+        })
+    })
+
+
 });
